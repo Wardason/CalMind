421555ac5f15346e3e43e9ba9f2f46ecc36b7aa2
leonardwarda <lenny.warda@icloud.com>
2025-03-30 09:16:37 +0200

    strucutre: task model/timeinfo

--- models/tasks.py
+++ models/tasks.py
@@ -1,7 +1,18 @@
-import dataclasses
+from dataclasses import dataclass, field
+import datetime
+from typing import Optional
 
 
-@dataclasses.dataclass
+@dataclass
+class TimeInfo:
+    date_time: datetime
+    time_zone: str
+
+@dataclass
 class Task:
     name: str
-    
+    start_time: TimeInfo
+    end_time: TimeInfo
+    priority: Optional[int] = None
+    description: Optional[str] = None
+    attendees: Optional[list[str]] = field(default_factory=list)
