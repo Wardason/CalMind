59a09198a6e12fbd1004e4a69237cb0f6c073950
leonardwarda <lenny.warda@icloud.com>
2025-03-31 10:46:37 +0200

    fix: openai requests relocating to openai_api.py

--- api/openai_api.py
+++ api/openai_api.py
@@ -1,1 +1,24 @@
-"""Handles openai api"""
+from openai import OpenAI
+
+from logic.utils import OPENAI_API_KEY
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def gpt_generate_response_request(prompt, user_input):
+    response = client.responses.create(
+        model="gpt-4o",
+        instructions=prompt,
+        input=user_input,
+    )
+    return response
+
+def gpt_structured_response_request(prompt, user_input):
+    response = client.responses.create(
+        model="gpt-4o-mini",
+        input=[
+            {"role": "system", "content": "Extract the event information."},
+            {"role": "user", "content": user_input}
+        ],
+        text=prompt
+    )
+    return response
--- logic/task_analysis.py
+++ logic/task_analysis.py
@@ -1,37 +1,23 @@
 import json
 from datetime import datetime
 import pytz
-from logic import utils
+
+from api.openai_api import gpt_generate_response_request, gpt_structured_response_request
+from logic.utils import structured_output_instructions_prompt, interpret_prompt
 from models.tasks import Task, TimeInfo
-from openai import OpenAI
-from logic.utils import OPENAI_API_KEY
 
 def interpret_user_input(user_input: str) -> str:
     tz = pytz.timezone("Europe/Berlin")
     now = datetime.now(tz).isoformat()
-    prompt: str = utils.interpret_prompt.replace("{{DATETIME_NOW}}", now).replace("{{TIMEZONE}}", tz.zone)
+    prompt: str = interpret_prompt.replace("{{DATETIME_NOW}}", now).replace("{{TIMEZONE}}", tz.zone)
 
-    client = OpenAI(api_key=OPENAI_API_KEY)
-    response = client.responses.create(
-        model="gpt-4o",
-        instructions=prompt,
-        input=user_input,
-    )
+    response = gpt_generate_response_request(prompt, user_input)
     return response.output_text
 
 def structured_output_from_user_input(user_input: str) -> dict:
-    client = OpenAI(api_key=OPENAI_API_KEY)
-    response = client.responses.create(
-        model="gpt-4o-mini",
-        input=[
-        {"role": "system", "content": "Extract the event information."},
-        {"role": "user", "content": user_input}
-    ],
-        text=utils.structured_output_instructions_prompt
-)
+    response = gpt_structured_response_request(structured_output_instructions_prompt, user_input)
     tasks = json.loads(response.output_text)
     return tasks
-
 
 def tasks_from_structured_output(data: dict) -> list[Task]:
         task_list = []
